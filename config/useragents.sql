5f4c7aaac346e3a358bda2df5477fade1cfac5f8
Pascal Bach <pascal.bach@siemens.com>
2011-06-27 08:05:18 +0200

    Add Opera 11.1x support Update current values for Firefox, Opera and Safari mobile

--- config/useragents.sql
+++ config/useragents.sql
@@ -7,7 +7,7 @@ INSERT INTO useragents (name, engine, version, active, current, popular, gbs, be
 INSERT INTO useragents (name, engine, version, active, current, popular, gbs, beta, mobile) VALUES ('Firefox 3.0', 'gecko', '^1.9.0', 1, 0, 1, 1, 0, 0);
 INSERT INTO useragents (name, engine, version, active, current, popular, gbs, beta, mobile) VALUES ('Firefox 3.5', 'gecko', '^1.9.1', 1, 0, 0, 1, 0, 0);
 INSERT INTO useragents (name, engine, version, active, current, popular, gbs, beta, mobile) VALUES ('Firefox 3.6', 'gecko', '^1.9.2', 1, 0, 1, 1, 0, 0);
-INSERT INTO useragents (name, engine, version, active, current, popular, gbs, beta, mobile) VALUES ('Firefox 4.0', 'gecko', '^2.0', 1, 1, 1, 1, 0, 0);
+INSERT INTO useragents (name, engine, version, active, current, popular, gbs, beta, mobile) VALUES ('Firefox 4.0', 'gecko', '^2.0', 1, 0, 1, 1, 0, 0);
 INSERT INTO useragents (name, engine, version, active, current, popular, gbs, beta, mobile) VALUES ('Firefox 5.0', 'gecko', '^5.0', 1, 1, 1, 1, 1, 0);
 
 -- Safari
@@ -27,7 +27,8 @@ INSERT INTO useragents (name, engine, version, active, current, popular, gbs, be
 INSERT INTO useragents (name, engine, version, active, current, popular, gbs, beta, mobile) VALUES ('Opera 10.20', 'presto', '^2.2.15$', 1, 0, 1, 0, 0, 0);
 INSERT INTO useragents (name, engine, version, active, current, popular, gbs, beta, mobile) VALUES ('Opera 10.5x', 'presto', '^2.5.', 1, 0, 1, 0, 0, 0);
 INSERT INTO useragents (name, engine, version, active, current, popular, gbs, beta, mobile) VALUES ('Opera 10.6x', 'presto', '^2.6.', 1, 0, 1, 0, 0, 0);
-INSERT INTO useragents (name, engine, version, active, current, popular, gbs, beta, mobile) VALUES ('Opera 11.0x', 'presto', '^2.7.', 1, 1, 1, 1, 0, 0);
+INSERT INTO useragents (name, engine, version, active, current, popular, gbs, beta, mobile) VALUES ('Opera 11.0x', 'presto', '^2.7.', 1, 0, 1, 1, 0, 0);
+INSERT INTO useragents (name, engine, version, active, current, popular, gbs, beta, mobile) VALUES ('Opera 11.1x', 'presto', '^2.8.', 1, 1, 1, 1, 0, 0);
 
 -- Chrome
 INSERT INTO useragents (name, engine, version, active, current, popular, gbs, beta, mobile) VALUES ('Chrome', 'chrome', '.*', 1, 1, 1, 1, 0, 0);
@@ -44,7 +45,7 @@ INSERT INTO useragents (name, engine, version, active, current, popular, gbs, be
 INSERT INTO useragents (name, engine, version, active, current, popular, gbs, beta, mobile) VALUES ('Mobile Safari 2.2.1', 'mobilewebkit', '^525', 1, 0, 0, 0, 0, 1);
 INSERT INTO useragents (name, engine, version, active, current, popular, gbs, beta, mobile) VALUES ('Mobile Safari 3.1.3', 'mobilewebkit', '^528', 1, 0, 0, 0, 0, 1);
 INSERT INTO useragents (name, engine, version, active, current, popular, gbs, beta, mobile) VALUES ('Mobile Safari 3.2', 'mobilewebkit', '^531', 1, 0, 0, 0, 1, 1);
-INSERT INTO useragents (name, engine, version, active, current, popular, gbs, beta, mobile) VALUES ('Mobile Safari 4.0.x', 'mobilewebkit', '^532', 1, 1, 0, 0, 1, 1);
+INSERT INTO useragents (name, engine, version, active, current, popular, gbs, beta, mobile) VALUES ('Mobile Safari 4.0.x', 'mobilewebkit', '^532', 1, 0, 0, 0, 1, 1);
 INSERT INTO useragents (name, engine, version, active, current, popular, gbs, beta, mobile) VALUES ('Mobile Safari 4.2', 'mobilewebkit', '^533', 1, 1, 0, 0, 0, 1);
 
 -- Windows Mobile
